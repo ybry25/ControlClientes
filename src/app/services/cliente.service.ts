f88cbf7f1573ff7da7c1e65f5cc2b03aba800bfc
Yhors Brayan <ybry25@gmail.com>
2023-07-15 17:10:26 -0500

    init

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from '../interfaces/cliente';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClienteService {
+  constructor() {}
+
+  updateCliente(id: number, cliente: Cliente) {
+    this.clientes[id] = cliente;
+  }
+
+  deleteCliente(id: number) {
+    this.clientes.splice(id, 1);
+  }
+
+  getClientes() {
+    return this.clientes;
+  }
+
+  getCliente(id: number) {
+    return this.clientes[id];
+  }
+
+  setCliente(listaCliente: Cliente[]) {
+    this.clientes = listaCliente;
+  }
+
+  private clientes: Cliente[] = [
+    {
+      nombre: 'Yhors Brayan',
+      apellido: 'X',
+      email: 'yhors@google.com',
+      saldo: 10,
+    },
+    {
+      nombre: 'Monica',
+      apellido: 'P',
+      email: 'moni@google.com',
+      saldo: 10,
+    },
+    {
+      nombre: 'juan',
+      apellido: 'peres',
+      email: 'jp@facebook.com',
+      saldo: 10,
+    },
+    {
+      nombre: 'Julian',
+      apellido: 'peres',
+      email: 'jjp@facebook.com',
+      saldo: 10,
+    },
+  ];
+}
